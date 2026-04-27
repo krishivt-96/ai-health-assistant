120d1a3fa0943509d5defd4c2cfc97e6ce797810
krishivt-96 <krishiv9605@gmail.com>
2026-04-27 15:43:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import google.generativeai as genai
 import os
-genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+genai.configure(api_key=st.secrets("GEMINI_API_KEY"))
 model = genai.GenerativeModel("gemini-pro")
 
 # --- Configuration ---
