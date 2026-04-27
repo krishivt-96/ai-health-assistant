e8b80e1d6cecd220c9621f8165b659261c781d3b
Krishiv T <krishivt@Krishivs-MacBook-Air.local>
2026-04-27 15:48:53 +0530

    fixed api key for streamlit

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import google.generativeai as genai
 import os
-genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+
+genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
 model = genai.GenerativeModel("gemini-pro")
 
 # --- Configuration ---
